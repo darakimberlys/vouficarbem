03994455015a3db658d1a4c0781ce9ee180b0f6d
Dara Kimberly <darakimberlys@hotmail.com>
2020-10-15 18:56:56 -0300

    first commit

---
+++ src/main/java/com/vouficarbem/controllers/HomeController.java
@@ -0,0 +1,13 @@
+package com.vouficarbem.controllers;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String index() {
+        System.out.println("Entrando no portal");
+        return "home";
+    }
+}
