671a45f79f42e3ecdb05129038bf14b707109bf7
Dara Kimberly <darakimberlys@hotmail.com>
2020-10-15 18:43:22 -0300

    first commit

---
+++ src/main/java/com/vouficarbem/VouFicarBemApplication.java
@@ -0,0 +1,13 @@
+package com.vouficarbem;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VouFicarBemApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VouFicarBemApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/vouficarbem/VouFicarBem/VouFicarBemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vouficarbem.VouFicarBem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VouFicarBemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
