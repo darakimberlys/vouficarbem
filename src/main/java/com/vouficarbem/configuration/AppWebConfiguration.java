ac7869f8df14d058e156b68b242263c9c3245084
Dara Kimberly <darakimberlys@hotmail.com>
2020-10-19 23:40:29 -0300

    configuration java

--- src/main/java/com/vouficarbem/configuration/AppWebConfiguration.java
+++ src/main/java/com/vouficarbem/configuration/AppWebConfiguration.java
@@ -1,8 +1,17 @@
 package com.vouficarbem.configuration;
 
 import com.vouficarbem.controllers.HomeController;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.format.datetime.DateFormatter;
+import org.springframework.format.datetime.DateFormatterRegistrar;
+import org.springframework.format.support.DefaultFormattingConversionService;
+import org.springframework.format.support.FormattingConversionService;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.multipart.MultipartResolver;
+import org.springframework.web.multipart.support.StandardServletMultipartResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
@@ -15,7 +24,37 @@ public class AppWebConfiguration {
         InternalResourceViewResolver resolver = new InternalResourceViewResolver();
         resolver.setPrefix("/WEB-INF/views/");
         resolver.setSuffix(".jsp");
-        resolver.setExposedContextBeanNames("carrinhoCompras");
+        resolver.setExposedContextBeanNames("acalme-se");
         return resolver;
+    }
+//  estrutura para auxílio de futuro form
+//    @Bean
+//    public MessageSource messageSource() {
+//        ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
+//        messageSource.setBasename("/WEB-INF/messages");
+//        messageSource.setDefaultEncoding("UTF-8");
+//        messageSource.setCacheSeconds(1);
+//
+//        return messageSource;
+//    }
+//
+//    @Bean
+//    public FormattingConversionService mvcConversionService() {
+//        DefaultFormattingConversionService conversionService = new DefaultFormattingConversionService();
+//        DateFormatterRegistrar registrar = new DateFormatterRegistrar();
+//        registrar.setFormatter(new DateFormatter("dd/MM/yyyy"));
+//        registrar.registerFormatters(conversionService);
+//
+//        return conversionService;
+//    }
+
+    @Bean
+    public MultipartResolver multipartResolver() {
+        return new StandardServletMultipartResolver();
+    }
+
+    @Bean
+    public RestTemplate restTemplate() {
+        return new RestTemplate();
     }
 }
---
+++ src/main/java/com/vouficarbem/configuration/JPAConfiguration.java
@@ -0,0 +1,48 @@
+package com.vouficarbem.configuration;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.orm.jpa.JpaTransactionManager;
+import org.springframework.orm.jpa.JpaVendorAdapter;
+import org.springframework.orm.jpa.LocalContainerEntityManagerFactoryBean;
+import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+
+import javax.persistence.EntityManagerFactory;
+
+@EnableTransactionManagement
+public class JPAConfiguration {
+
+    @Bean
+    public LocalContainerEntityManagerFactoryBean entityManagerFactory() {
+
+        LocalContainerEntityManagerFactoryBean factoryBean = new LocalContainerEntityManagerFactoryBean();
+
+        JpaVendorAdapter vendorAdapter = new HibernateJpaVendorAdapter();
+        factoryBean.setJpaVendorAdapter(vendorAdapter);
+
+//        Preparação para o banco de dados
+
+//        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+//        dataSource.setUsername("root");
+//        dataSource.setPassword("");
+//        dataSource.setUrl("jdbc:mysql://localhost:3307/vouficarbem?useSSL=false&serverTimezone=UTC");
+//        dataSource.setDriverClassName("com.mysql.cj.jdbc.Driver");
+//        factoryBean.setDataSource(dataSource);
+//
+//        Properties properties = new Properties();
+//
+//        properties.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
+//        properties.setProperty("hibernate.show_sql", "true");
+//        properties.setProperty("hibernate.hbm2ddl.auto", "update");
+//
+//        factoryBean.setJpaProperties(properties);
+//        factoryBean.setPackagesToScan("br.com.vouficarbem.loja.models");
+//
+        return factoryBean;
+    }
+
+    @Bean
+    public JpaTransactionManager transactionManager(EntityManagerFactory emf) {
+        return new JpaTransactionManager(emf);
+    }
+}
